5071fa786cbb79bed2199ec50cbbc701056fc3dc
ngminhat187 <ngminhat187@gmail.com>
2024-07-13 22:19:49 +0700

    learn: get to know useRef hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import { UseLayoutEffect } from "./react-hooks";
+import { UseRef } from "./react-hooks";
 
 function App() {
   return (
     <div className="App">
-      <UseLayoutEffect />
+      <UseRef />
     </div>
   );
 }
--- src/react-hooks/index.js
+++ src/react-hooks/index.js
@@ -1,3 +1,4 @@
 export { default as UseState } from "./useState/UseState";
 export { default as UseEffect } from "./useEffect/UseEffect";
 export { default as UseLayoutEffect } from "./useLayoutEffect/UseLayoutEffect";
+export { default as UseRef } from "./useRef/UseRef";
