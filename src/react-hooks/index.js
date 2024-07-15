27742fccc9814de0ee65135ad629300beb400456
ngminhat187 <ngminhat187@gmail.com>
2024-07-15 22:56:07 +0700

    learn: get to know useMemo hook

--- src/react-hooks/index.js
+++ src/react-hooks/index.js
@@ -3,3 +3,4 @@ export { default as UseEffect } from "./useEffect/UseEffect";
 export { default as UseLayoutEffect } from "./useLayoutEffect/UseLayoutEffect";
 export { default as UseRef } from "./useRef/UseRef";
 export { default as UseCallback } from "./useCallback/UseCallback";
+export { default as UseMemo } from "./useMemo/UseMemo";
