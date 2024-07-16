a0a61836a41bd91239cb7af27c6614ca5d7452e9
ngminhat187 <ngminhat187@gmail.com>
2024-07-17 02:01:25 +0700

    chore: move useEffect hook to new branch

--- src/hoc/memo/components/ShowTime.js
+++
@@ -1,8 +0,0 @@
-import { memo } from "react";
-
-function ShowTime({ second }) {
-  console.log("render ShowTime");
-  return <h1>Time: {second}</h1>;
-}
-
-export default memo(ShowTime);
--- src/react-hooks/useCallback/components/Count.js
+++
@@ -1,8 +0,0 @@
-import { memo } from "react";
-
-function Count({ onIncrease }) {
-  console.log("render Count");
-  return <button onClick={onIncrease}>Increase</button>;
-}
-
-export default memo(Count);
